50bb14d9e547d6c5f460e3dcd5d2cfe0aed38546
Matheus Grassi Milanezi <matheus.milanezi@aeromot.com.br>
2022-10-14 17:07:53 -0300

    aula 25

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Models\Event;
+use App\Models\User;
 use Illuminate\Support\Facades\Redirect;
 use Symfony\Component\VarDumper\Caster\RedisCaster;
 
@@ -50,6 +51,10 @@ class EventController extends Controller
 
        }
 
+       $user = auth()->user();
+       $event->user_id = $user->id;
+
+
        $event->save();
        return redirect('/')->with('msg', "Evento criado com sucesso!");
 
@@ -59,6 +64,130 @@ class EventController extends Controller
         
         $event = Event::findOrFail($id);
 
-        return view('events.show', ['event' => $event]);
+        // $user = auth()->user();
+        // $hasUserJoined = false;
+
+        // if($user) {
+
+        //     $userEvents = $user->eventsAsParticipant->toArray();
+
+        //     foreach($userEvents as $userEvent) {
+        //         if($userEvent['id'] == $id) {
+        //             $hasUserJoined = true;
+        //         }
+        //     }
+
+        // }
+
+        $eventOwner = User::where('id', $event->user_id)->first()->toArray();
+
+        // return view('events.show', ['event' => $event, "eventOwner" => $eventOwner, 'hasUserJoined' => $hasUserJoined]);
+        return view('events.show', ['event' => $event, "eventOwner" => $eventOwner]);
     }
+
+    public function dashboard(){
+
+        $user = auth()->user();
+
+        $events = $user->events;
+
+        return view('events.dashboard', ['events' => $events]);
+    }
+
+
+    // public function dashboard() {
+
+    //     $user = auth()->user();
+
+    //     $events = $user->events;
+
+    //     $eventsAsParticipant = $user->eventsAsParticipant;
+
+    //     return view('events.dashboard', 
+    //         ['events' => $events, 'eventsasparticipant' => $eventsAsParticipant]
+    //     );
+
+    // }
+
+    // public function destroy($id) {
+
+    //     Event::findOrFail($id)->delete();
+
+    //     return redirect('/dashboard')->with('msg', 'Evento excluído com sucesso!');
+
+    // }
+
+    // public function edit($id) {
+
+    //     $user = auth()->user();
+
+    //     $event = Event::findOrFail($id);
+
+    //     if($user->id != $event->user_id) {
+    //         return redirect('/dashboard');
+    //     }
+
+    //     return view('events.edit', ['event' => $event]);
+
+    // }
+
+    // public function update(Request $request) {
+
+    //     $data = $request->all();
+
+    //     // Image Upload
+    //     if($request->hasFile('image') && $request->file('image')->isValid()) {
+
+    //         $requestImage = $request->image;
+
+    //         $extension = $requestImage->extension();
+
+    //         $imageName = md5($requestImage->getClientOriginalName() . strtotime("now")) . "." . $extension;
+
+    //         $requestImage->move(public_path('img/events'), $imageName);
+
+    //         $data['image'] = $imageName;
+
+    //     }
+
+    //     Event::findOrFail($request->id)->update($data);
+
+    //     return redirect('/dashboard')->with('msg', 'Evento editado com sucesso!');
+
+    // }
+
+    // public function joinEvent($id) {
+
+    //     $user = auth()->user();
+
+    //     $user->eventsAsParticipant()->attach($id);
+
+    //     $event = Event::findOrFail($id);
+
+    //     return redirect('/dashboard')->with('msg', 'Sua presença está confirmada no evento ' . $event->title);
+
+    // }
+
+    // public function leaveEvent($id) {
+
+    //     $user = auth()->user();
+
+    //     $user->eventsAsParticipant()->detach($id);
+
+    //     $event = Event::findOrFail($id);
+
+    //     return redirect('/dashboard')->with('msg', 'Você saiu com sucesso do evento: ' . $event->title);
+
+    // }
+
+    public function login()
+    {
+        return view('auth.login');
+    }
+
+    public function register()
+    {
+        return view('auth.register');
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,17 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EventController;
 
 Route::get('/', [EventController::class, 'index']);
-Route::get('/events/create', [EventController::class, 'create']);
+Route::get('/events/create', [EventController::class, 'create'])->middleware('auth');
 Route::get('/events/{id}', [EventController::class, 'show']);
 Route::post('/events', [EventController::class, 'store']);
+
+Route::delete('/events/{id}', [EventController::class, 'destroy'])->middleware('auth');
+Route::get('/events/edit/{id}', [EventController::class, 'edit'])->middleware('auth');
+Route::put('/events/update/{id}', [EventController::class, 'update'])->middleware('auth');
+
+Route::get('/dashboard', [EventController::class, 'dashboard'])->middleware('auth');
+Route::post('/events/join/{id}', [EventController::class, 'joinEvent'])->middleware('auth');
+Route::delete('/events/leave/{id}', [EventController::class, 'leaveEvent'])->middleware('auth');
 
 Route::get('/sobre', function () {
     return view('sobre');
@@ -28,4 +36,15 @@ Route::get('/contact', function(){
     return view('contact');
 });
 
+// Route::middleware([
+//     'auth:sanctum',
+//     config('jetstream.auth_session'),
+//     'verified'
+// ])->group(function () {
+//     Route::get('/dashboard', function () {
+//         return view('dashboard');
+//     })->name('dashboard');
+// });
 
+Route::get('/login', [EventController::class, 'login']);
+Route::get('/register', [EventController::class, 'register']);
